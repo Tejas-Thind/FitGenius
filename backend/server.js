93933b038598ae9ca6851d600f9c993c3fc50bab
Tejas.T <155028169+Tejas-3105@users.noreply.github.com>
2024-07-26 08:08:59 +0000

    .env file

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
+require('dotenv').config();
+
 const express = require('express');
 
 // Express app
 const app = express();
 
+// Routes
+app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to the app!' });
+});
+
 // Listen for requests
-app.listen(4000, () => {
-    console.log('Listening on port 4000')
+app.listen(process.env.PORT, () => {
+    console.log('Listening on port', process.env.PORT)
 });
