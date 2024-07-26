f7c88159550b29a024f14f3ce1c6f2a306114357
Tejas.T <155028169+Tejas-3105@users.noreply.github.com>
2024-07-26 19:01:44 +0000

    installing mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,37 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const workoutRoutes = require('./routes/workouts')
+const express = require("express");
+const workoutRoutes = require("./routes/workouts");
+const mongoose = require("mongoose");
 
 // Express app
 const app = express();
 
 // Middleware
-app.use(express.json(), express.urlencoded({extended: true}));
+app.use(express.json(), express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-    console.log(req.path, res.method, res.statusCode);
-    next();
+  console.log(req.path, res.method, res.statusCode);
+  next();
 });
 
 // Routes
-app.use('/api/workouts',workoutRoutes);
+app.use("/api/workouts", workoutRoutes);
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.DB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((result) => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 // Listen for requests
 app.listen(process.env.PORT, () => {
-    console.log('Listening on port', process.env.PORT)
+  console.log("Listening on port", process.env.PORT);
 });
