8d776125dcf5cdd1fd2614e84bb9cd93b1c3ac4a
Tejas.T <155028169+Tejas-3105@users.noreply.github.com>
2024-07-26 18:43:36 +0000

    CRUD routes

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,5 +1,28 @@
 const router = require('express').Router();
 
+// GET all workouts
+router.get('/', (req, res) => {
+    res.json({ mssg: 'Workouts endpoint' });
+});
 
+// GET a single workout
+router.get('/:id', (req, res) => {
+    //req.params.id
+})
+
+// POST a new wokrout
+router.post('/', (req, res) => {
+
+});
+
+// DELETE a workout
+router.delete('/:id', (req, res) => {
+
+});
+
+// PATCH a workout
+router.patch('/:id', (req, res) => {
+
+});
 
 module.exports = router;
