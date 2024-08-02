d65b7f50185b05f74e351a275796cde52864e734
Tejas.T <155028169+Tejas-3105@users.noreply.github.com>
2024-08-02 06:35:49 +0000

    making homepage

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,27 @@
+import { useState, useEffect } from "react";
+
 const Home = () => {
+
+  const [workouts, setWorkouts] = useState([]);
+  useEffect(() => {
+    document.title = "FitGenius | Home";
+    const fetchWorkouts = async () => {
+      const response = await fetch("http://localhost:4000/api/workouts");
+      const data = await response.json();
+      
+      if (response.ok) {
+        setWorkouts(data);
+      }
+      else {
+        console.log('There was an error');
+      }
+    };
+    fetchWorkouts();
+  }, []);
+
   return (
-    <div className="home">
-      <h2>Home</h2>
+    <div className="workouts">
+      
     </div>
   );
 };
