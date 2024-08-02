39e6cd716d04a57f014a7ce6c71ce15efde60c72
Tejas.T <155028169+Tejas-3105@users.noreply.github.com>
2024-08-02 06:41:43 +0000

    creating homepage, and fetching data

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,27 +1,32 @@
 import { useState, useEffect } from "react";
 
 const Home = () => {
-
   const [workouts, setWorkouts] = useState([]);
   useEffect(() => {
     document.title = "FitGenius | Home";
     const fetchWorkouts = async () => {
-      const response = await fetch("http://localhost:4000/api/workouts");
+      const response = await fetch(
+        "https://shiny-parakeet-g44wqw4g4gjvfwj7v.github.dev/api/workouts"
+      );
       const data = await response.json();
-      
+
       if (response.ok) {
         setWorkouts(data);
-      }
-      else {
-        console.log('There was an error');
+      } else {
+        console.log("There was an error");
       }
     };
     fetchWorkouts();
   }, []);
 
   return (
-    <div className="workouts">
-      
+    <div className="home">
+      <div className="workouts">
+        {workouts.map((workout) => {
+          <workoutDetails workout={workout} key={workout._id} />;
+        })}
+        ;
+      </div>
     </div>
   );
 };
