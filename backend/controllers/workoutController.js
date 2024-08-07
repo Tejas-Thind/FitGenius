b5ae8927a77ce06d57515fec3dad5ff1b2405228
Tejas.T <155028169+Tejas-3105@users.noreply.github.com>
2024-08-07 20:46:25 +0000

    added better looking delete button, some functionality is still not working.

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -46,7 +46,7 @@ const createWorkout = async (req, res) => {
   }
 
   if(emptyFields.length > 0) {
-    return res.status(400).json({error: 'Please fill in all the required fields: ' + emptyFields.join(', ')});
+    return res.status(400).json({ error: `Please fill in all fields: ${emptyFields.join(', ')}`, emptyFields })
   }
 
   // Add workout to DB
--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,4 +1,19 @@
-const workoutDetails = ({ workout }) => {
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+
+const WorkoutDetails = ({ workout }) => {
+  const { dispatch } = useWorkoutsContext();
+
+  const handleClick = async () => {
+    const response = await fetch("/api/workouts/" + workout._id, {
+      method: "DELETE",
+    });
+    const json = await response.json();
+
+    if (response.ok) {
+      dispatch({ type: "DELETE_WORKOUT", payload: json });
+    }
+  };
+
   return (
     <div className="workout-details">
       <h3>{workout.title}</h3>
@@ -18,9 +33,15 @@ const workoutDetails = ({ workout }) => {
         <strong>Notes: </strong>
         {workout.notes}
       </p>
-      <p><strong>Created at: </strong>{workout.createdAt}</p>
+      <p>
+        <strong>Created at: </strong>
+        {workout.createdAt}
+      </p>
+      <span className="material-symbols-outlined" onClick={handleClick}>
+        delete
+      </span>
     </div>
   );
 };
 
-export default workoutDetails;
+export default WorkoutDetails;
