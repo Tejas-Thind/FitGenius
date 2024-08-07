02f13d43659d0dc999655626d963bdfb6ca8328a
Tejas.T <155028169+Tejas-3105@users.noreply.github.com>
2024-08-07 20:57:13 +0000

    fixed error of not reloading page after deleting

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -10,7 +10,7 @@ const WorkoutDetails = ({ workout }) => {
     const json = await response.json();
 
     if (response.ok) {
-      dispatch({ type: "DELETE_WORKOUT", payload: json });
+      dispatch({ type: "DELETE_WORKOUT", payload: json.workout });
     }
   };
 
